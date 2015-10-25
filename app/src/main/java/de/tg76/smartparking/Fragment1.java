c323265edf7c2d7d80674f0f453be368c52af50d
Toto <graebi@gmail.com>
2015-10-25 11:27:43 +0000

    removed classes (code which retrieves carpark space)

--- app/src/main/java/de/tg76/smartparking/Fragment1.java
+++ app/src/main/java/de/tg76/smartparking/Fragment1.java
@@ -11,8 +11,6 @@ public class Fragment1 extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_fragment1, container, false);
-
-
     }
 
 
